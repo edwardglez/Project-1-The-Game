9ecedc0b9b83407cbf813deef3606dc8bab1e548
edwardglez <71733757+edwardglez@users.noreply.github.com>
2020-10-12 00:30:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,126 +1,79 @@
-const statusDiv = document.querySelector('.status');
-const resetDiv = document.querySelector('.reset');
-const cellDivs = document.querySelectorAll('.game-cell');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
+body {
+  color: #545454;
+  background-color: white;
+  display: flex;
+  font-family: sans-serif;
+  justify-content: center;
+}
 
-const xSymbol = 'Player 1';
-const oSymbol = 'Player 2';
+.container {
+  margin: 50px;
+  padding: 50px;
+  border-radius: 25px;
+}
 
+.title {
+  text-align: center;
+}
 
-let gameIsLive = true;
-let xIsNext = true;
+.status-action {
+  display: flex;
+  margin-top: 25px;
+  font-size: 25px;
+  justify-content: space-around;
+  height: 30px;
+}
 
+.reset {
+  cursor: pointer;
+}
 
+.reset:hover {
+  color: red;
+}
 
-const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+.game-grid {
+  background: black;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 15px;
+  margin-top: 50px;
+}
 
-const handleWin = (letter) => {
-    gameIsLive = false;
-    if (letter === 'x') {
-        statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
-    } else {
-        statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
-    }
-};
+.game-cell {
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 200px;
+  width: 200px;
+}
 
-const checkGameStatus = () => {
-    const topLeft = cellDivs[0].classList[1];
-    const topMiddle = cellDivs[1].classList[1];
-    const topRight = cellDivs[2].classList[1];
-    const middleLeft = cellDivs[3].classList[1];
-    const middleMiddle = cellDivs[4].classList[1];
-    const middleRight = cellDivs[5].classList[1];
-    const bottomLeft = cellDivs[6].classList[1];
-    const bottomMiddle = cellDivs[7].classList[1];
-    const bottomRight = cellDivs[8].classList[1];
+.x {
+  background-image: url('./X.gif');
+}
+.o {
+  cursor: default;
+  background-image: url('./O.gif');
+}
 
+.game-cell {
+    height: 150px;
+    width: 150px;
+  }
 
-    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[1].classList.add('won');
-        cellDivs[2].classList.add('won');
-    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-        handleWin(middleLeft);
-        cellDivs[3].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[5].classList.add('won');
-    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-        handleWin(bottomLeft);
-        cellDivs[6].classList.add('won');
-        cellDivs[7].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[3].classList.add('won');
-        cellDivs[6].classList.add('won');
-    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-        handleWin(topMiddle);
-        cellDivs[1].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[7].classList.add('won');
-    } else if (topRight && topRight === middleRight && topRight === bottomRight) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-        cellDivs[5].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[6].classList.add('won');
-    } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
-        gameIsLive = false;
-        statusDiv.innerHTML = 'Draw';
-    } else {
-        xIsNext = !xIsNext;
-        if (xIsNext) {
-            statusDiv.innerHTML = `${xSymbol} your turn`;
-        } else {
-            statusDiv.innerHTML = `<span>${oSymbol} your turn</span>`;
-        }
-    }
-};
+  .x::after {
+    font-size: 150px;
+  }
 
-
-// event Handlers
-const handleReset = () => {
-    xIsNext = true;
-    statusDiv.innerHTML = `${xSymbol} is next`;
-    for (const cellDiv of cellDivs) {
-        cellDiv.classList.remove('x');
-        cellDiv.classList.remove('o');
-        cellDiv.classList.remove('won');
-    }
-    gameIsLive = true;
-};
-
-const handleCellClick = (e) => {
-    const classList = e.target.classList;
-
-    if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
-        return;
-    }
-
-    if (xIsNext) {
-        classList.add('x');
-        checkGameStatus();
-    } else {
-        classList.add('o');
-        checkGameStatus();
-    }
-};
-
-
-// event listeners
-resetDiv.addEventListener('click', handleReset);
-
-for (const cellDiv of cellDivs) {
-    cellDiv.addEventListener('click', handleCellClick)
+  .o::after {
+    font-size: 175px;
 }
